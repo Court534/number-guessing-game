167fa4899ac9e33cf8468705ee573ac62d2c7ba3
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-24 16:20:18 +0100

    starting working on the logic

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 "use strict";
 
-console.log(document.querySelector(".message"));
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = 
+"🎉 Correct!"
+
+document.querySelector(".number").textContent = 13
+document.querySelector(".score").textContent = 20
+
+document.querySelector(".guess").
+
