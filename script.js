d4e7af5b279de328a49f080a30739f598cbfe0b2
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-26 18:18:03 +0100

    Almost finished the game logic

--- script.js
+++ script.js
@@ -15,22 +15,31 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
+let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess);
-  console.log(document.querySelector(".score").textContent);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No number entered! ⛔️";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct!";
+    highScore += score;
+    document.querySelector(".highscore").textContent = highScore;
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Too High! 📈";
-    document.querySelector(".score").value - 1
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High! 📈";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You ran out of guesses, Game Over!💥";
+      score = 0
+      document.querySelector(".score").textContent = score;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Too Low! 📉"
+    document.querySelector(".message").textContent = "Too Low! 📉";
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 });
-
-// 'Too high!' : '📉 Too low!'
