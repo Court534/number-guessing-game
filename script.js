af27ef9bc8b64cb33b8ab4f6fe87cbb4ec26f5ff
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-26 16:33:31 +0100

    Building out game logic

--- script.js
+++ script.js
@@ -13,6 +13,24 @@
 // document.querySelector(".guess").value = 21
 // console.log(document.querySelector(".guess").value = 21);
 
-document.querySelector(".check").addEventListener("click", function() {
-  console.log(document.querySelector(".guess").value);
-})
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
+  console.log(document.querySelector(".score").textContent);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number entered! ⛔️";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High! 📈";
+    document.querySelector(".score").value - 1
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too Low! 📉"
+  }
+});
+
+// 'Too high!' : '📉 Too low!'
