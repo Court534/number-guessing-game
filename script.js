d155cd3d8451fce7a87607273dedc27612d07e2c
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-30 15:04:30 +0100

    finished the final application

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// Generating the secret number 
+// Generating the secret number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = "?";
 let score = 20;
@@ -17,8 +17,12 @@ document.querySelector(".check").addEventListener("click", function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct!";
-    highScore += score;
-    document.querySelector(".highscore").textContent = highScore;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").textContent = secretNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
 
     // Guess too high indicator
   } else if (guess > secretNumber) {
@@ -33,6 +37,7 @@ document.querySelector(".check").addEventListener("click", function () {
         "You ran out of guesses, Game Over!💥";
       score = 0;
       document.querySelector(".score").textContent = score;
+      document.querySelector("body").style.backgroundColor = "red";
     }
 
     // Guess too low indicator
@@ -48,6 +53,24 @@ document.querySelector(".check").addEventListener("click", function () {
         "You ran out of guesses, Game Over!💥";
       score = 0;
       document.querySelector(".score").textContent = score;
+      document.querySelector("body").style.backgroundColor = "red";
     }
   }
+});
+
+// Again function
+document.querySelector(".again").addEventListener("click", function () {
+  let score = 20;
+
+  const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector(".message").textContent = "Start guessing...";
+
+  document.querySelector(".score").textContent = score;
+
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "black";
 });
