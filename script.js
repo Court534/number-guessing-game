f88dc5e12445c526bc35b6f5ee5b3c84ff2c2d8e
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-30 13:39:45 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 "use strict";
 
+// Generating the secret number 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = "?";
 let score = 20;
 let highScore = 0;
 
+// Game Logic
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // When no number is entered
   if (!guess) {
     document.querySelector(".message").textContent = "No number entered! ⛔️";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct!";
     highScore += score;
     document.querySelector(".highscore").textContent = highScore;
+
+    // Guess too high indicator
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too High! 📈";
       score--;
       document.querySelector(".score").textContent = score;
+
+      // Game over
     } else {
       document.querySelector(".message").textContent =
         "You ran out of guesses, Game Over!💥";
       score = 0;
       document.querySelector(".score").textContent = score;
     }
+
+    // Guess too low indicator
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too Low! 📉";
       score--;
       document.querySelector(".score").textContent = score;
+
+      // Game over
     } else {
       document.querySelector(".message").textContent =
         "You ran out of guesses, Game Over!💥";
